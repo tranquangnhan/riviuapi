afb23e73885a185ce6dd76f913a5b9eb102f03fe
tranquangnhan <tranquangnhan1606@gmail.com>
2020-12-24 11:27:32 +0700

    first commit

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+var admin = require("firebase-admin");
+
+var serviceAccount = require("../firebase/serviceAccountKey.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://apiplace-299105-default-rtdb.firebaseio.com"
+});
+
+var db = admin.database();
+
+
+router.get('/', function(req, res, next) {
+
+    var ref = db.ref('/users/' + 'nhan');
+    ref.once("value", function(snapshot) {
+        // console.log(snapshot.val());
+        res.json(snapshot.val());
+    });
+});
+
+module.exports = router;
